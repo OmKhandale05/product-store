e56a95654f9d861a5d080d06387e87fbeb473b32
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-14 21:14:44 +0530

    Feature: Add back button on products details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import api from "../services/api";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 const ProductDetails = () => {
 
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     api.get(`/products/${id}`)
@@ -19,7 +21,18 @@ const ProductDetails = () => {
     return <div className="text-center mt-20">Loading product...</div>;
   }
   return (
-    <div className="p-10 max-w-5xl mx-auto grid md:grid-cols-2 gap-10">
+    <div className="p-10 max-w-5xl mx-auto ">
+        <button
+        onClick={() => navigate(-1)}
+        className="flex items-center gap-2 mb-6 border px-4 py-2 rounded-lg hover:bg-gray-100"
+        >
+            <ArrowLeftIcon className="w-5 h-5"/>
+            Back
+        </button>
+
+      <div className="grid md:grid-cols-2 gap-10">
+
+    </div>  
       <div className="flex justify-center">
         <img
           src={product.image}
