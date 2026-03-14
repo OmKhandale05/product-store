f16a4d3c0feae0ea710ecf235053cab446811086
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-14 19:34:29 +0530

    Feature: Add loading state while fetching products with axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,27 @@ import api from "../services/api";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     api
       .get("/products")
       .then((res) => {
         setProducts(res.data);
+        setLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {console.log(err);
+      setLoading(false);
+      });
   }, []);
+
+  if(loading) {
+    return (
+        <div className="text-center mt-20 text-xl font-semibold">
+            Loading products...
+        </div>
+    )
+  }
   return (
     <div className="p-8 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {products.map((product) => (
